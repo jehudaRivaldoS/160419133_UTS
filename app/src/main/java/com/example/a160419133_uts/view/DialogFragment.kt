4363c61c141b4948c018fec27d44268cbc81dea7
jehudaRivaldoS <71364423+jehudaRivaldoS@users.noreply.github.com>
2021-10-07 12:05:43 +0700

    Push 2

--- app/src/main/java/com/example/a160419133_uts/view/DialogFragment.kt
+++ app/src/main/java/com/example/a160419133_uts/view/DialogFragment.kt
@@ -23,11 +23,6 @@ class DialogFragment : BottomSheetDialogFragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        btnSuccess.setOnClickListener {
-            val action = DialogFragmentDirections.actionDialogBack()
-            Navigation.findNavController(it).navigate(action)
-        }
     }
 
 }
